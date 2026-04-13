568cefae740ead425ab5021185e21d6fb39a65db
0xrhma <0xrhma@protonmail.com>
2026-04-13 21:31:58 +0545

    Add files via upload

--- kv.h
+++ kv.h
@@ -13,16 +13,17 @@ typedef unsigned long ulong;
 typedef const char* string;
 
 struct KvObj {
-  string* keys;
+  string** keys;
   struct KvValue* values;
   ulong count; // total key entries
   // ulong ksize; // total size consumed by keys in bytes
 };
 
 struct KvValue{
-  string* entries;
-  struct KvObj* obj; 
+  string** entries;
+  struct KvObj obj; 
   ulong count; // total value entries
+  bool hasobj;
   // ulong vsize; // total size consumed by values in bytes
 };
 
@@ -36,8 +37,9 @@ typedef struct KvValue KvValue;
 
 // not required to be called bofore in a single threaded usecase
 void KvInit();
-KvObj* KvLoadFile(const char *path);
+int KvLoadFile(struct KvObj* pobj, const char* path);
 void KvObjFree(struct KvObj* pobj);
+string KvObjGetKeyIndex(struct KvObj* pobj, ulong index);
 struct KvValue* KvObjGetValue(struct KvObj* pobj, string key);
 
 // KvValue methods
--- main.c
+++ main.c
@@ -1,38 +1,18 @@
 #include "kv.h"
-#include <stdio.h>
-
-void pkv(KvObj* obj, uint ts) {
-  for (uint i = 0; i < obj->count; i++) {
-    for (uint tc=0; tc<ts; tc++)
-      fputc(' ', stdout);
-    fprintf(stdout, "%s : ", obj->keys[i]);
-    KvValue* cobj = obj->values + i;
-    if (cobj->obj) {
-      fputc('\n', stdout);
-      pkv(cobj->obj, ts+2);
-    }
-    else {
-      for (uint l=0; l<cobj->count; l++)
-        fprintf(stdout, " %s ", cobj->entries[l]);
-    }
-    fputc('\n', stdout);
-  }
-}
-
 
 int main(int argc, char* const argv[]) {
   if (argc < 2)
     return -1;
-  KvObj* res = KvLoadFile(argv[1]);
-  if (!res) 
+  KvObj res;
+  if (!KvLoadFile(&res, argv[1]))
     return -1;
-  // KvValue *val = KvObjGetValue(res, "themes");
-  // val = KvObjGetValue(val->obj, "dark");
-  // val = KvObjGetValue(val->obj, "color");
-  
-  // fprintf(stdout, "%s\n", val->entries[0]);
-  pkv(res, 0);
-
-  KvObjFree(res);
+  // KvValue *val = KvObjGetValue(&res, "font_family");
+  // if (!val)
+  //   return -1;
+  // if (!val->obj) {
+  //   string font_color = "";
+  //   printf("font_family : %s\n", font_color);
+  // }
+  // KvObjFree(&res);
   return 0;
 }
